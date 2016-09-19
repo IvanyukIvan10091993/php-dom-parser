63f4a1e79dc36630e468bc2ed42cf65fc41dcea0
ivanivanyuk <von.asar@gmail.com>
2016-09-19 19:29:17 +0300

    Actual version

--- DomParser.php
+++ DomParser.php
@@ -2,63 +2,122 @@
 
 class DomParser
 {
-    private static $domObjInsertors = [
-        'a' => 'SELF::insertA',
-        'li' => 'SELF::insertLi',
-        'ul' => 'SELF::insertUl',
+    private static $domObjInsertorsCategory = [
+        'a' => 'insertAnchorCategory',
+        'li' => 'insertLiCategory',
+        'ul' => 'insertUl',
+    ];
+    private static $domObjInsertorsItems = [
     ];
 
-    private static function computeDomObj(&$objXpath, &$urlString)
+    private $categoryXpath = '';
+    private $itemsXpath = '';
+    private $paginationXpath = '';
+    private $urlString = '';
+    private $waitTime = 0;
+
+    private function computeDomObjects($xpath, $urlString)
     {
-        return SELF::computeXpath($urlString)->query($objXpath)[0];
+        $domObjects = [];
+        $domNodeList = $this->computeXpath($urlString)->query($xpath);
+        foreach ($domNodeList as $domObject) {
+            $domObjects[] = $domObject;
+        }
+        return $domObjects;
     }
-    private static function computeXpath(&$urlString)
+    private function computeXpath($urlString)
     {
+        sleep($this->waitTime); // Эта строчка здесь для того, чтобы сервер не решил, что мы бот
         $dom = new DOMDocument();
         $dom->preserveWhiteSpace = false;
         @$dom->loadHTML(file_get_contents($urlString));
         return new DOMXPath($dom);
     }
-    private static function insertA(&$data, &$domObj)
+    private function computeLastPageNumber($urlString)
+    {
+        123 // Should check if pagen found
+        echo $urlString;
+        return array_pop(explode('=', $this->computeDomObjects($this->paginationXpath, $urlString)[0]->getAttribute('href')));
+    }
+    private function hasNoSubcategories(&$dataObj)
+    {
+        return (!array_key_exists('subcategories', $dataObj)) ? true : false;
+    }
+    private function insertAnchorCategory(&$data, $domObj)
     {
         $data['name'] = $domObj->nodeValue;
         $data['href'] = $domObj->getAttribute('href');
     }
-    private static function insertDomObj(&$data, &$domObj) {
-        if (array_key_exists($domObj->nodeName, SELF::$domObjInsertors)) {
-            call_user_func_array(SELF::$domObjInsertors[$domObj->nodeName], [&$data, &$domObj]);
+    private function insertDomObjCategory(&$data, $domObj) {
+        if (array_key_exists($domObj->nodeName, SELF::$domObjInsertorsCategory)) {
+            call_user_func_array([$this, SELF::$domObjInsertorsCategory[$domObj->nodeName]], [&$data, $domObj]);
         }
     }
-    private static function insertLi(&$data, &$domObj)
+    private function insertDomObjItems(&$data, $domObj) {
+        if (array_key_exists($domObj->nodeName, SELF::$domObjInsertorsItems)) {
+            call_user_func_array([$this, SELF::$domObjInsertorsItems[$domObj->nodeName]], [&$data, $domObj]);
+        }
+    }
+    private function insertLiCategory(&$data, $domObj)
     {
         $dataObj = [];
         foreach ($domObj->childNodes as $child) {
-            SELF::insertDomObj($dataObj, $child);
+            $this->insertDomObjCategory($dataObj, $child);
+        }
+        if ($this->hasNoSubcategories($dataObj)) {
+            $this->parseItems($dataObj);
         }
         $data[] = $dataObj;
     }
-    private static function insertUl(&$data, &$domObj)
+    private function insertUl(&$data, $domObj)
     {
         $dataObj = [];
         foreach ($domObj->childNodes as $child) {
-            SELF::insertDomObj($dataObj, $child);
+            $this->insertDomObjCategory($dataObj, $child);
         }
-        $data['children'] = $dataObj;
+        $data['subcategories'] = $dataObj;
+    }
+    private function parseItems(&$dataObj)
+    {
+        $itemsUrlString = rtrim($this->urlString, "/") . $dataObj['href'];
+        $lastPageNumber = $this->computeLastPageNumber($itemsUrlString);
+        for ($i = 1, $len = $lastPageNumber + 1; $i < $len; $i++) {
+            $url = $itemsUrlString . '?sort=PROPERTY_ostatok&method=DESC&PAGEN_1=' . $i;
+            $domObjects = $this->computeDomObjects($this->itemsXpath, $url);
+            foreach ($domObjects as $domObject) {
+                echo "<pre>";
+                echo $i;
+                echo "\n";
+                print_r($domObject->getAttribute('href'));
+                echo "</pre>";
+                flush();
+                ob_flush();
+            }
+        }
+        //die();
     }
 
-    function parseMenu($objXpath, $urlString)
+    public function __construct($categoryXpath, $itemsXpath, $paginationXpath, $waitTime, $urlString)
     {
+        $this->categoryXpath = $categoryXpath;
+        $this->itemsXpath = $itemsXpath;
+        $this->urlString = $urlString;
+        $this->waitTime = $waitTime;
+        $this->paginationXpath = $paginationXpath;
+    }
+    public function parseCategory()
+    {
+        set_time_limit(0); // Эта строчка здесь для того, чтобы php не прекращал выполнение скрипта после стандартных 30 секунд работы
         $data = [];
-        $domObj = SELF::computeDomObj($objXpath, $urlString);
+        $domObjects = $this->computeDomObjects($this->categoryXpath, $this->urlString);
 
-        SELF::insertDomObj($data, $domObj);
-
-        echo '<pre>';
-        print_r($data['children']);
-        echo '</pre>';
+        foreach ($domObjects as $domObject) {
+            $this->insertDomObjCategory($data, $domObject);
+        }
     }
 }
 
-DomParser::parseMenu('//*[@id="tabs1_"]/ul', 'http://www.duim24.ru/');
+$parser = new DomParser('//*[@id="tabs1_"]/ul', '//*[contains(@class, "tovar-col tovar2")]/a', '//*[contains(@class, "pager-last")]/a', 1, 'http://www.duim24.ru/');
+$parser->parseCategory();
 
 ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require('DomParser.php');
+
+?>
